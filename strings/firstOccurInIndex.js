4f4fd8e390c4f0c6564d7d57f101963297263124
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-03-02 16:34:52 +0530

    string1 in string2

---
+++ strings/firstOccurInIndex.js
@@ -0,0 +1,16 @@
+let string1 = "sadbutsad";
+let string2 = "sad";
+
+var strStr = function(string1, string2){
+    const n = string1.length;
+    const m = string2.length;
+
+    for(let i=0; i<=n-m; i++){
+        if(string1.substring(i, i+m) === string2){
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(strStr(string1, string2));
