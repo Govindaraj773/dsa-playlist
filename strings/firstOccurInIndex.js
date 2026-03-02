f65831f3ab45378b1b81596cd50dbbd31f8d27f5
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-03-02 16:56:11 +0530

    Using brute force

--- strings/firstOccurInIndex.js
+++ strings/firstOccurInIndex.js
@@ -1,16 +1,40 @@
 let string1 = "sadbutsad";
 let string2 = "sad";
 
-var strStr = function(string1, string2){
-    const n = string1.length;
-    const m = string2.length;
+// // Method 1 : Using substring
+// var strStr = function(string1, string2){
+//     const n = string1.length;
+//     const m = string2.length;
 
-    for(let i=0; i<=n-m; i++){
-        if(string1.substring(i, i+m) === string2){
-            return i;
+//     for(let i=0; i<=n-m; i++){
+//         if(string1.substring(i, i+m) === string2){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// console.log(strStr(string1, string2));  
+
+
+// Method 2 : Brute force method
+
+var strStr2 = function(string1, string2){
+    if(string2.length === 0) return 0;
+
+    for(let i=0; i<string1.length - string2.length; i++){
+        let match = true;
+
+        for(let j=0; j<string2.length; j++){
+            if(string1[i+j] !== string2[j]){
+                match = false;
+                break;
+            }
         }
+        if(match) return i;
     }
     return -1;
 }
 
-console.log(strStr(string1, string2));  
+console.log(strStr2(string1, string2));
+
