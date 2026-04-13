4606e048adb5ebc8997a13a5fc944ffa7d100cd1
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-04-13 19:18:17 +0530

    last word length

--- strings/lastWordLength.js
+++ strings/lastWordLength.js
@@ -1,20 +1,20 @@
-let s = "Good Morning";
+let s = "Good Morning all";
 
 function lastWordLength(s) {
-    let count = 0;
-    for (let i = s.length - 1; i >= 0; i--) {
-        if (s[i] === ' ' && count === 0) {
-            continue;
-        }
-
-        if (s[i] === ' ') {
-            break;
-        }
-
-        count++;
+  let count = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " " && count === 0) {
+      continue;
     }
 
-    return count;
+    if (s[i] === " ") {
+      break;
+    }
+
+    count++;
+  }
+
+  return count;
 }
 
 console.log(lastWordLength(s));
