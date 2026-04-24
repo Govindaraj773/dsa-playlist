86e897e6ad3fb761f6f19e06696ae3c44c846df4
Govindaraj Cholappagol <govindaraj5547@gmail.com>
2026-04-24 12:00:06 +0530

    finding unique value in array

---
+++ arrays/findUniqueValue.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 3, 4, 3, 2, 1, -3];
+
+let bag = {};
+
+for (let i = 0; i < arr.length; i++) {
+  let ele = arr[i];
+  if (bag[ele] === undefined) {
+    bag[ele] = 1;
+  } else {
+    bag[ele]++;
+  }
+}
+// console.log(bag); //finding the unique value in the array
+
+for (let key in bag) {
+  if (bag[key] === 1) {
+    console.log(key);
+  }
+}
